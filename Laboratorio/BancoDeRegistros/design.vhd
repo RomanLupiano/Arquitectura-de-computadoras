80182b3b1efe1e37514ea4b2c72d02352813e03f
RomanLupiano <romanlupiano@gmail.com>
2025-04-21 18:44:34 -0300

    Banco de Registros arreglado

--- Laboratorio/BancoDeRegistros/design.vhd
+++ Laboratorio/BancoDeRegistros/design.vhd
@@ -1,41 +1,47 @@
 -- Code your design here
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
 use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
 entity BancoRegistros is
-	port ( 
-    	clk: in std_logic;
-    	reset: in std_logic;
-        reg1_rd: in std_logic_vector(4 downto 0);
-        reg2_rd: in std_logic_vector(4 downto 0);
-        wr: in std_logic;
-        reg_wr: in std_logic_vector(4 downto 0);
-        data_wr: in std_logic_vector(31 downto 0);
-        data1_rd: out std_logic_vector(31 downto 0);
-        data2_rd: out std_logic_vector(31 downto 0)
+	port (
+    	clk : in std_logic;
+        reset : in std_logic;
+        wr : in std_logic;
+        reg1_rd : in std_logic_vector(4 downto 0); 
+        reg2_rd : in std_logic_vector(4 downto 0); 
+        reg_wr : in std_logic_vector(4 downto 0); 
+        data_wr : in std_logic_vector(31 downto 0);
+        data1_rd : out std_logic_vector(31 downto 0);
+        data2_rd : out std_logic_vector(31 downto 0)
     );
 end BancoRegistros;
 
 architecture BancoRegistros_arch of BancoRegistros is
-type tBanco is array(0 to 31) of std_logic_vector(31 downto 0);
-signal Regs: tBanco;
+
+type tBanco is Array(0 to 31) of std_logic_vector(31 downto 0);
+signal Regs : tBanco;
+
 begin
-    -- If reg1_rd!= 0 then data1_rd<= Regs(reg1_rd) else data1_rd<= x”00000000”
-    -- If reg2_rd!= 0 then data2_rd<= Regs(reg2_rd) else data2_rd<= x”00000000”
-    -- If falling_edge(clk) and wr=’1’ then Regs(reg_wr) <= data_wr;
+	-- conv_integer(std_logic_vector) devuelve el número del vector en tipo integer, ya que el arreglo requiere un indice en integer para poder recorrerlo.
+	-- Lectura del registro apuntado por reg1_rd
+    data1_rd <= Regs(conv_integer(reg1_rd)) when reg1_rd /= "00000"
+    		else x"00000000";
     
-    -- conv_integer(std_logic_vecto) -> pasa un std_logic_vector a un integer.
-    data1_rd <= Regs(conv_integer(reg1_rd)) when reg1_rd /= "00000" else x"00000000";
-    data2_rd <= Regs(conv_integer(reg2_rd)) when reg2_rd /= "00000" else x"00000000";
+    -- Lectura del registro apuntado por reg2_rd
+	data2_rd <=  Regs(conv_integer(reg2_rd)) when reg2_rd /= "00000"
+			else x"00000000";    
     
-    process
+    -- Escritura de registros y reset
+	escritura: process (reset, clk, wr, data_wr) 
     begin
-      if (clk'event and clk='0' and wr='1') then
-       Regs(conv_integer(reg_wr)) <= data_wr;
-      end if;
+    	if (reset = '1') then
+            Regs <= (others => ( others => '0')); -- Todos los registros toman el valor x"00000000"
+        end if;
+        
+        if (clk'event and clk='0' and wr='1') then 
+    		Regs(conv_integer(reg_wr)) <= data_wr;
+    	end if;
     end process;
-    
-    
 end BancoRegistros_arch;
--- Laboratorio/BancoDeRegistros/testbench.vhd
+++ Laboratorio/BancoDeRegistros/testbench.vhd
@@ -2,36 +2,84 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity tb_BancoRegistros is
-end tb_BancoRegistros;
+entity registers_tb is
+end entity;
 
-architecture BancoRbegistros_tb of tb_BancoRegistros is
+architecture tb of registers_tb is
+
 component BancoRegistros is
-	port ( 
-    	clk: in std_logic;
-    	reset: in std_logic;
-        reg1_rd: in std_logic_vector(4 downto 0);
-        reg2_rd: in std_logic_vector(4 downto 0);
-        wr: in std_logic;
-        reg_wr: in std_logic_vector(4 downto 0);
-        data_wr: in std_logic_vector(31 downto 0);
-        data1_rd: out std_logic_vector(31 downto 0);
+	port (
+    	clk : in std_logic;
+        reset : in std_logic;
+        wr : in std_logic;
+        reg1_rd : in std_logic_vector(4 downto 0); 
+        reg2_rd : in std_logic_vector(4 downto 0); 
+        reg_wr : in std_logic_vector(4 downto 0); 
+        data_wr : in std_logic_vector(31 downto 0);
+        data1_rd : out std_logic_vector(31 downto 0);
         data2_rd: out std_logic_vector(31 downto 0)
     );
 end component;
-signal reg1_rd, reg2_rd, reg_wr: std_logic_vector(4 downto 0);
-signal data_wr, data1_rd, data2_rd: std_logic_vector(31 downto 0);
-signal reset, clk, wr: std_logic;
-begin
-	uut: BancoRegistros port map (clk=>clk, reset=>reset, reg1_rd=>reg1_rd, reg2_rd=>reg2_rd, wr=>wr, reg_wr=>reg_wr, data_wr=>data_wr, data1_rd=>data1_rd, data2_rd=>data2_rd);
-    
-    clk_proc: process
-    begin
-        clk <= '1';
-        wait for 5 ns;
-        wait;
-    end process;
-    
-	
 
-end BancoRbegistros_tb;
+signal clk : std_logic;
+signal reset : std_logic;
+signal wr : std_logic;
+signal reg1_rd : std_logic_vector(4 downto 0); 
+signal reg2_rd : std_logic_vector(4 downto 0); 
+signal reg_wr : std_logic_vector(4 downto 0); 
+signal data_wr : std_logic_vector(31 downto 0);
+signal data1_rd : std_logic_vector(31 downto 0);
+signal data2_rd	: std_logic_vector(31 downto 0);
+
+begin
+	uut: BancoRegistros
+    	port map (
+        	clk => clk,
+            reset => reset,
+            wr => wr,
+            reg1_rd => reg1_rd,
+            reg2_rd => reg2_rd,
+            reg_wr => reg_wr,
+            data_wr => data_wr,
+            data1_rd => data1_rd,
+            data2_rd => data2_rd
+        );
+        
+    process
+    begin
+    	clk <= '0';
+      	wait for 1 ns;
+      	clk <= '1';
+    	wait for 1 ns;
+    end process;
+
+    int_main : process
+    begin
+      	reg1_rd <= "00000";
+      	reg2_rd <= "00000";
+        reg_wr <= "00000";
+        reset <= '0';
+        wr <= '0';
+        data_wr <= x"00000000";            
+    	wait for 1 ns;
+		
+		wr <= '1';
+        data_wr <= x"0000FFFF";            
+		reg_wr <= "00011";
+        wait for 4 ns;
+        
+        reg1_rd <= "00011";
+        data_wr <= x"FFFF0000";            
+		reg_wr <= "11000";
+        wait for 4 ns;
+
+        reg2_rd <= "11000";
+        wait for 4 ns;
+
+		wr <= '0';
+        reset <= '1';
+
+      wait;
+    end process;
+
+end tb;
